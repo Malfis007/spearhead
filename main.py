c920de4fa7560d8a13900b789a49d8ff96a6f48a
Malfis007 <malfis007@yahoo.com>
2022-04-11 13:31:32 -0500

    change to using lsof

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 import socket
-#import thread
+import os
 from psutil import process_iter
 from signal import SIGTERM
 
 # check for open ports
 def portScan():
+    #ip = input('Enter a scan target: ')
+    ip = '127.0.0.1'
     for port in range(0, 65536):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # specific to this device
-        result = sock.connect_ex(('127.0.0.1', port))
+        #result = sock.connect_ex(('127.0.0.1', port))
+        result = sock.connect_ex((ip, port))
 
         # if port is open
         if (result == 0):
@@ -23,14 +26,13 @@ def portScan():
         sock.close()
 
 # kill processes
-# proc.connections cant detect rdp, only tcp that rdp rides on
-# consider replacement
 def procKill():
     for proc in process_iter():
         try: 
-            for conn in proc.connections(kind='inet'):
+            for conn in proc.connections():
                 print(f'Killing RDP process on port {conn.laddr.port}')
                 #proc.send_signal(SIGTERM)
+                os.system(f'lsof -i :{conn.laddr.port}')
         except:
             print(proc)
 
